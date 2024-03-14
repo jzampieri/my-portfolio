1b817fb4d57115ced828104de09f029de8b7f27e
Julio <juliocezampieri@gmail.com>
2024-03-14 18:32:38 -0300

    grid template area - ABOUT

--- src/components/Transition/Transition.jsx
+++ src/components/Transition/Transition.jsx
@@ -6,10 +6,18 @@ const Transition = () => {
     return(
         <section className='container-transition'>
             <div className='cards'>
-                <div className='regiao'></div>
-                <div className=''></div>
-                <div className=''></div>
-                <div className=''></div>
+                <div className='apresentacao'>
+                    <h1>I live in Sao Paulo, Brazil 📍</h1>
+                </div>
+                <div className='imagem1'></div>
+                <div className='c-gray'></div>
+                <div className='c-orange'></div>
+                <div className='imagem2'></div>
+                <div className='imagem3'></div>
+                <div className='imagem4'></div>
+                <div className='imagem5'></div>
+                <div className='imagem6'></div>
+                <div className='imagem7'></div>
             </div>
         </section>
     )
