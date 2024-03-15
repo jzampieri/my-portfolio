c8f33f4f4bd752ec03d0520fb55c23755c7ea3d5
Julio <juliocezampieri@gmail.com>
2024-03-15 00:37:43 -0300

    Colors grid template

--- src/components/Transition/Transition.jsx
+++ src/components/Transition/Transition.jsx
@@ -7,14 +7,26 @@ const Transition = () => {
         <section className='container-transition'>
             <div className='cards'>
                 <div className='apresentacao'>
-                    <h1>I live in Sao Paulo, Brazil 📍</h1>
+                    <h1>Hi, 👋  my name is Julio Cesar Zampieri.</h1>
+                    <h1>I live in Sao Paulo, Brazil. 📍</h1>
+                    <h1>18 years old.</h1>
                 </div>
                 <div className='imagem1'></div>
-                <div className='c-gray'></div>
-                <div className='c-orange'></div>
+                <div className='c-gray'>
+                    <h1>" Technology moves the world "</h1>
+                    <p>Steve jobs is my inspiration.</p>
+                    <h2>- Steve Jobs</h2>
+                </div>
+                <div className='c-orange'>
+                    <h1>" Technology is my passion "</h1>
+                    <p> Codding too. </p>
+                    <h2>- Julio Zampieri 😬</h2>
+                </div>
                 <div className='imagem2'></div>
                 <div className='imagem3'></div>
-                <div className='imagem4'></div>
+                <div className='imagem4'>
+                    <h1>I'm a software engineer academic at FIAP. Actually, I'm in my 3nd semester.</h1>
+                </div>
                 <div className='imagem5'></div>
                 <div className='imagem6'></div>
                 <div className='imagem7'></div>
