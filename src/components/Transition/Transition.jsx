3aeacdbfd64b4741b2408a33dd353aebc8410e7f
Julio <juliocezampieri@gmail.com>
2024-03-16 20:17:27 -0300

    Troca da paleta de cores

--- src/components/Transition/Transition.jsx
+++ src/components/Transition/Transition.jsx
@@ -1,5 +1,8 @@
 import {} from 'react'
 import './transition.css'
+import { LuGithub } from "react-icons/lu";
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 
 const Transition = () => {
 
@@ -9,6 +12,7 @@ const Transition = () => {
                 <div className='apresentacao'>
                     <h1>Hi, 👋  my name is Julio Cesar Zampieri.</h1>
                     <h1>I live in Sao Paulo, Brazil. 📍</h1>
+                    <h1>Full Stack Developer.</h1>
                     <h1>18 years old.</h1>
                 </div>
                 <div className='imagem1'></div>
@@ -22,14 +26,37 @@ const Transition = () => {
                     <p> Codding too. </p>
                     <h2>- Julio Zampieri 😬</h2>
                 </div>
-                <div className='imagem2'></div>
+                <div className='imagem2'>
+
+                </div>
                 <div className='imagem3'></div>
                 <div className='imagem4'>
                     <h1>I'm a software engineer academic at FIAP. Actually, I'm in my 3nd semester.</h1>
                 </div>
-                <div className='imagem5'></div>
-                <div className='imagem6'></div>
-                <div className='imagem7'></div>
+                <div className='imagem5'>
+
+                </div>
+                <div className='imagem6'>
+                    <h1>See my social medias :</h1>
+                </div>
+                <div className='imagem7'>
+                    <div className='icons'>
+                        <a href="https://github.com/jzampieri">
+                            <LuGithub/>
+                        </a>
+                    </div>
+                    <div className='icons'>
+                        <a href="https://www.instagram.com/j.zampierii/">
+                            <FaInstagram/>
+                        </a>
+                    </div>
+                    <div className='icons'>
+                        <a href="https://www.linkedin.com/in/julio-cesar-zampieri-3257b6267/">
+                            <FaLinkedin />
+                        </a>
+                    </div>
+
+                </div>
             </div>
         </section>
     )
